33e72763e188c953e4d4d2b24e238a7ef4d70c54
Autoratch <autorockza@gmail.com>
2020-01-12 21:25:36 +0700

    Better ver for Treap (support multiset)

--- treap.cpp
+++ treap.cpp
@@ -3,60 +3,55 @@ using namespace std;
 
 struct node
 {
-    int size,val,prio;
+    int sz,val,prio;
     node *l,*r;
 };
 
 using pnode = node*;
 
-int sz(pnode t){ if(t) return t->size; else return 0; }
-void updt(pnode t){ if(t) t->size = sz(t->l)+sz(t->r)+1; }
+int sz(pnode t){ if(!t) return 0; return t->sz; }
+void upd(pnode t){ if(!t) return; t->sz = 1+sz(t->l)+sz(t->r); }
 
 void split(pnode t,pnode &l,pnode &r,int key)
 {
-    if(!t) l = r = NULL;
-    else if(t->val<=key) split(t->r,t->r,r,key),l = t;
+    if(!t) return void(l = r = NULL);
+    if(key>=t->val) split(t->r,t->r,r,key),l = t;
     else split(t->l,l,t->l,key),r = t;
-    updt(t);
+    upd(t);
 }
 
 void merge(pnode &t,pnode l,pnode r)
 {
-    if(!l or !r){ if(!l) t = r; else t = l; }
-    else if(l->prio>r->prio) merge(l->r,l->r,r),t = l;
+    if(!l) return void(t = r);
+    if(!r) return void(t = l);
+    if(l->prio>r->prio) merge(l->r,l->r,r),t = l;
     else merge(r->l,l,r->l),t = r;
-    updt(t);
+    upd(t);
 }
 
 void insert(pnode &t,pnode it)
 {
-    if(!t) t = it;
-    else if(it->prio>t->prio) split(t,it->l,it->r,it->val),t = it;
-    else 
-    {
-        if(it->val>t->val) insert(t->r,it);
-        else insert(t->l,it);
-    }
-    updt(t);
+    if(!t) return void(t = it);
+    if(it->prio>t->prio) split(t,it->l,it->r,it->val),t = it;
+    else if(it->val>t->val) insert(t->r,it);
+    else insert(t->l,it);
+    upd(t);
 }
 
 void erase(pnode &t,int key)
 {
     if(!t) return;
-    else if(t->val==key){ pnode tmp = t; merge(t,t->l,t->r); free(tmp); }
-    else
-    {
-        if(key>t->val) erase(t->r,key);
-        else erase(t->l,key);
-    }
-    updt(t);
+    if(key==t->val){ pnode tmp = t; merge(t,t->l,t->r); free(tmp); }
+    else if(key>t->val) erase(t->r,key);
+    else erase(t->l,key);
+    upd(t);
 }
 
-pnode init(int val)
+pnode init(int key)
 {
-    pnode ret = new node;
-    *ret = {1,val,rand(),NULL,NULL};
-    return ret;
+    pnode res = new node;
+    *res = {1,key,rand(),NULL,NULL};
+    return res;
 }
 
 int main()
