dce91e5b214b59247fa39c178181109b0737c215
Autoratch <autorockza@gmail.com>
2020-03-28 13:09:51 +0700

    update dp opt dividenconquer

--- dp_opt_dividenconquer.cpp
+++ dp_opt_dividenconquer.cpp
@@ -10,7 +10,7 @@ int dp[2][N];
 
 int c(int x,int y);
 
-void compute(int l,int r,int optl,int optr,int f)
+void compute(int l,int r,int optl,int optr,int f,int id)
 {
     if(l>r) return;
     int mid = (l+r)/2;
@@ -20,9 +20,10 @@ void compute(int l,int r,int optl,int optr,int f)
         int now = dp[f^1][k]+c(k,mid);
         if(now<val) val = now,opt = k;
     }
+    // id is i, and mid is j, for dp(i,j)
     dp[f][mid] = val; // opt of k is opt
-    compute(l,mid-1,optl,opt,f);
-    compute(mid+1,r,opt,optr,f);
+    compute(l,mid-1,optl,opt,f,id);
+    compute(mid+1,r,opt,optr,f,id);
 }
 
 int main()
