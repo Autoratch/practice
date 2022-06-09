fcbc89b891764f2af6f071682fadad4423f334b4
Krittitee Ratchanan <autorockza@gmail.com>
2022-06-09 18:17:45 +0700

    Update bellman_ford.cpp

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -24,7 +24,7 @@ int main()
     dist[0] = 0;
     for(int i = 0;i < n-1;i++) for(int j = 0;j < m;j++)
     {
-        int a = adj[i].second.first,b = adj[i].second.second,d = adj[i].first;
+        int a = adj[j].second.first,b = adj[j].second.second,d = adj[j].first;
         if(dist[a]!=INT_MAX and dist[a]+d<dist[b]) dist[b] = dist[a]+d;
     }
 
